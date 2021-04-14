d380a93643f7c388a7b74486946477b430534d62
apyavchik <pyavchik@gmail.com>
2021-04-14 12:59:12 +0300

    add line formating

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,14 +4,14 @@ public class App {
     public static void main(String[] args) {
         ArrayList<Cat> cats = new ArrayList<>();
         Cat tomCat = new Cat("Tom", "gray", 3.5, 2, false);
-        Cat matroskinCat = new Cat("Matroskin","black and white", 4.2, 5, true);
-        Cat barsikCat = new Cat("Barsic","white", 3.2, 1, true);
+        Cat matroskinCat = new Cat("Matroskin", "black and white", 4.2, 5, true);
+        Cat barsikCat = new Cat("Barsic", "white", 3.2, 1, true);
 
         cats.add(tomCat);
         cats.add(matroskinCat);
         cats.add(barsikCat);
 
-        for (Cat cat: cats) {
+        for (Cat cat : cats) {
             cat.eat();
             cat.sleep();
         }
